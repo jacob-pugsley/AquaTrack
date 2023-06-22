35ec3f58278226a5cc4d49eebea4e2d7c9a84933
BuildTools <unconfigured@null.spigotmc.org>
2023-06-22 14:02:36 -0500

    added tank creation and details views

--- app/src/main/java/com/example/aquatrack/MainActivity.kt
+++ app/src/main/java/com/example/aquatrack/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.example.aquatrack
 
 import android.os.Bundle
+import android.widget.RadioGroup
+import android.widget.Spinner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -21,15 +27,24 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.aquatrack.ui.theme.AquaTrackTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,29 +55,41 @@ class MainActivity : ComponentActivity() {
             AquaTrackTheme {
                 // A surface container using the 'background' color from the theme
                 val navController = rememberNavController()
+                val tankList = remember { mutableListOf<TankData>() }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
 
                     NavHost(navController = navController, startDestination = "main") {
-                        composable("main") { Main(navController = navController) }
-                        composable("createNewTank") { CreateNewTank() }
-                        /*...*/
+                        composable("main") { Main(navController, tankList) }
+                        composable("createNewTank") { CreateNewTank(navController, tankList) }
+                        composable("tankDetails/{tankname}", arguments =
+                        listOf(navArgument("tankname") {type = NavType.StringType})) {
+                            ViewTankDetails(navController = navController, tank = getTankByName( tankList, it.arguments?.getString("tankname")))
+                        }
                     }
                 }
             }
-
-
         }
     }
 }
 
+fun getTankByName(tankList: MutableList<TankData>, name: String?) : TankData? {
+    return tankList.find { td -> td.name == name }
+}
+
+class TankData(
+    val name: String,
+    val length: Int?,
+    val width: Int?,
+    val height: Int?,
+    val gallons: Int,
+    val waterType: String
+)
+
 @Composable
-fun Main(navController: NavHostController) {
-
-
-    val tankList = remember { mutableListOf("Tank 1", "Tank 2", "Tank 3") }
+fun Main(navController: NavHostController, tankList: MutableList<TankData>) {
 
     Column (
         verticalArrangement = Arrangement.spacedBy(50.dp)
@@ -70,13 +97,17 @@ fun Main(navController: NavHostController) {
         Text("AquaTrack", style = TextStyle(fontSize = 50.sp))
 
         tankList.map {
-            Button(onClick = { navController.navigate("createNewTank") }) {
-                Text(text = "$it >", style = TextStyle(fontSize = 50.sp))
+            Button(
+                onClick = { navController.navigate("tankDetails/${it.name}") },
+                shape = RectangleShape,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "${it.name} (${it.gallons}G) >", style = TextStyle(fontSize = 50.sp))
             }
         }
 
 
-        Text("+ Add new tank", style= TextStyle(fontSize = 50.sp), modifier = Modifier.clickable(true, onClick = {navController.navigate("createNewTank")}))
+        Text("+ Add new tank", style= TextStyle(fontSize = 50.sp, color = Color.Blue, textDecoration = TextDecoration.Underline), modifier = Modifier.clickable(true, onClick = {navController.navigate("createNewTank")}))
     }
 
 
@@ -85,45 +116,56 @@ fun Main(navController: NavHostController) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CreateNewTank() {
+fun CreateNewTank(navController: NavHostController, tankList: MutableList<TankData>) {
     var length by remember { mutableStateOf("") }
     var width by remember { mutableStateOf("") }
     var height by remember { mutableStateOf("") }
-    var gallons by remember {
-        mutableStateOf("")
-    }
+    var gallons by remember { mutableStateOf("") }
     var knowGallons by remember {
         mutableStateOf(true)
     }
-    Column {
-        Text("Create a new tank")
+    var name by remember {
+        mutableStateOf("")
+    }
+    val waterTypes = listOf("Freshwater", "Saltwater")
+    var selectedWaterType by remember {
+        mutableStateOf("Freshwater")
+    }
+    Column (
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("Create a new tank", style=MaterialTheme.typography.headlineLarge)
+
+        Text("Tank name", style=MaterialTheme.typography.headlineMedium)
+        OutlinedTextField(value = name, onValueChange = {name = it})
 
         Row {
             Switch(checked = knowGallons, onCheckedChange = { knowGallons = it })
-            Text("I know how many gallons my tank is.")
+            Text("I know how many gallons my tank is.", style=MaterialTheme.typography.headlineMedium)
         }
 
         if( knowGallons ) {
-            Text("Gallons")
+            Text("Gallons", style=MaterialTheme.typography.headlineSmall)
             OutlinedTextField(
                 value = gallons,
                 onValueChange = { gallons = it },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
         }else {
-            Text("Length (left to right)")
+            Text("Length (in)", style=MaterialTheme.typography.headlineSmall)
             OutlinedTextField(
                 value = length,
                 onValueChange = { length = it },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
-            Text("Width (front to back)")
+            Text("Width (in)", style=MaterialTheme.typography.headlineSmall)
             OutlinedTextField(
                 value = width,
                 onValueChange = { width = it },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
-            Text("Height (top to bottom)")
+            Text("Height (in)", style=MaterialTheme.typography.headlineSmall)
             OutlinedTextField(
                 value = height,
                 onValueChange = { height = it },
@@ -131,8 +173,53 @@ fun CreateNewTank() {
             )
             gallons = calculateGallons(length, width, height).toString()
             Text(
-                "Gallons: $gallons"
+                "Gallons: $gallons", style=MaterialTheme.typography.headlineSmall
             )
+        }
+
+        Text("Water type", style=MaterialTheme.typography.headlineMedium)
+        waterTypes.forEach {
+            Row {
+                RadioButton(selected = selectedWaterType == it , onClick = { selectedWaterType = it })
+                Text(it, style = MaterialTheme.typography.headlineSmall)
+            }
+        }
+
+        Button(onClick = {
+            tankList.add(
+                TankData(
+                    name,
+                    length.toIntOrNull(),
+                    width.toIntOrNull(),
+                    height.toIntOrNull(),
+                    gallons.toInt(),
+                    selectedWaterType
+                )
+            )
+            navController.navigate("main")
+        }) {
+            Text("Create tank")
+        }
+    }
+}
+
+@Composable
+fun ViewTankDetails(navController: NavHostController, tank: TankData?) {
+    if( tank == null ) {
+        Text("No tank specified.")
+    } else {
+        Column {
+            Text(tank.name, style = MaterialTheme.typography.headlineLarge)
+            Text("Volume: ${tank.gallons}G", style=MaterialTheme.typography.headlineMedium)
+            if (tank.length != null && tank.width != null && tank.height != null) {
+                Text("Dimensions: ${tank.length}\"L x ${tank.width}\"W x ${tank.height}\"H",
+                    style=MaterialTheme.typography.headlineMedium)
+            } else {
+                Text("Dimensions not specified.",
+                    style = MaterialTheme.typography.headlineMedium
+                        .merge(TextStyle(fontStyle = FontStyle.Italic)))
+            }
+            Text(tank.waterType, style=MaterialTheme.typography.headlineMedium)
         }
     }
 }
